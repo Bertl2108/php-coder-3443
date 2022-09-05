9eefd3c5616039eb5915f4a8b91898f88a865aa1
Bertl2108 <86019213+Bertl2108@users.noreply.github.com>
2022-09-05 13:46:32 +0200

    Add files via upload

---
+++ Vorlagen_OOP/index.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'config.php';
+require_once 'functions.php';
+
+$conn = connectToDb();
+
+
+
+// put your code here
+closeDb($conn);
+?>
+
+<!DOCTYPE html>
+<html>    
+    <head>
+        <meta charset="UTF-8">
+        <title>title</title>
+    </head>
+    
+    <body>
+        <h1>title</h1>
+        <div>
+            
+        </div>       
+    </body>    
+</html>
